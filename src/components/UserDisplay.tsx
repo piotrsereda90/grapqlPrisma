869523734dea8554bd38768bfede4ecfccf15636
p.sereda <piotr.sereda@dentsu.com>
2023-04-26 11:05:22 +0200

    add Branch component to project

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -6,6 +6,7 @@ type Props = {
 }
 
 function UserDisplay({user}: Props){
+    console.log(';lllllllllllllllllll',user)
     return  <div className="flex gap-x-24 justify-center">
                 <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
                     <p className="text-xl text-gray-200 font-bold">
